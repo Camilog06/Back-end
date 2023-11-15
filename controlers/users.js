3d2488631a21ccd292bee9c7c35e25038263da4c
Camilog06 <camilogoez06@gmail.com>
2023-11-15 15:59:34 -0500

    API Complete

--- controlers/users.js
+++ controlers/users.js
@@ -3,55 +3,78 @@ import { validateUser, validatePartialUser } from "../schemes/users.js";
 
 export class UserController {
     static async getAll(req, res) {
-        if (req.mysql) {
-          const users = await UserModel.getAll({ pool: req.mysql });
-          if (users) return res.json(users);
-        }
-        res.status(404).json({ message: 'User not found' });
-      }
-
-    static async getById (req, res) {
-        const { id } = req.params
-        const users = await UserModel.getById({ id })
-        if (users) return res.json(users)
-        res.status(404).json({message: 'User not found'})
-    }
-
-    static async create(req, res) {
         try {
-            const input = req.body; 
-            const newUser = await UserModel.create({ input });
-    
-            res.status(201).json(newUser);
+            const users = await UserModel.getAll({ pool: req.mysql });
+            if (users) {
+                return res.json(users);
+            } else {
+                return res.status(404).json({ message: 'Users not found' });
+            }
         } catch (error) {
-            console.error(error);
+            console.error('Error al obtener todos los usuarios:', error);
             res.status(500).json({ error: 'Error interno del servidor' });
         }
     }
     
-    static async delete (req, res)  {
-        const { id } = req.params
-    
-        const result = await UserModel.delete({ id })
-    
-        if (result == false) {
-            return res.status(404).json({ message: 'User not found' })
-        }
-    
-        return res.json({ message: 'User deleted' })
-    }
 
-    static async update (req, res) {
-        const result = validatePartialUser(req.body)
+    static async create({ input, pool }) {
+        try {
+          if (!input) {
+            throw new Error('Input is undefined');
+          }
+      
+          const result = await pool.query(
+            'INSERT INTO user (name, dob, adress, description, createdAt) VALUES (?, ?, ?, ?, ?);',
+            [input.nombre, input.dob, input.adress, input.description, input.createdAt]
+          );
+      
+          const newUser = {
+            id: result[0].insertId,
+            ...input,
+          };
+      
+          return newUser;
+        } catch (error) {
+          console.error('Error al crear un usuario:', error);
+          throw error;
+        }
+      }      
+    
+      static async delete(req, res) {
+        const { name } = req.params;
+        console.log('Deleting user with name:', name);
+
+        try {
+            const result = await UserModel.deleteByName({ name, pool: req.mysql });
+
+            if (result === false) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            return res.json({ message: 'User deleted' });
+        } catch (error) {
+            console.error('Error deleting user:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }    
+    
+
+    static async update(req, res) {
+        const result = validatePartialUser(req.body);
     
         if (!result.success) {
-            return res.status(400).json({ error: json.parse(result.error.message) })
+            return res.status(400).json({ error: json.parse(result.error.message) });
         }
     
-        const { id } = req.params
-        
-        const updateUser = await UserModel.update({ id, input: result.data })
+        const { name } = req.params;
+        const pool = req.mysql;
     
-        return res.json(updateUser)
-    }
+        if (!pool) {
+            return res.status(500).json({ error: 'Internal Server Error - Database pool not available' });
+        }
+    
+        const updateUser = await UserModel.updateByName({ name, input: result.data, pool });
+    
+        return res.json(updateUser);
+    }    
 }
